8837e3af6979f2fbbd6096c7e0828d75a01cb960
Harrison Itotia <harrisonitotia@gmail.com>
2023-10-01 16:26:12 +0300

    install react-router-dom and create the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import './App.css'
+import { Home } from './components/Home'
 import { Login } from './components/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Logout } from './components/Logout'
+import { Notifications } from './components/Notifications'
+import { Receipts } from './components/Receipts'
+import { Profile } from './components/Profile'
 
 function App() {
   
@@ -7,6 +13,16 @@ function App() {
   return (
     <>
       <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/notifications" element={<Notifications/>} />
+          <Route path="/receipts" element={<Receipts/>} />
+          <Route path="/logout" element={<Logout/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
