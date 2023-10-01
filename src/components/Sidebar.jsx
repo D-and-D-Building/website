ddb815267ac97bc80016412af634df12d2bf5ba2
Harrison Itotia <harrisonitotia@gmail.com>
2023-10-01 16:30:15 +0300

    create sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <>
+      <div className="">
+        <img src="" alt="logo" />
+      </div>
+
+      <div className="">
+        <ul>
+          <li>Profile</li>
+          <li>Receipts</li>
+          <li>Notifications</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+    </>
   )
 }
