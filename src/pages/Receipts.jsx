ea69ed06dccdecaf88db9b9c9331327b4a0f3c35
Harrison Itotia <harrisonitotia@gmail.com>
2023-10-02 23:19:43 +0300

    create files

--- src/pages/Receipts.jsx
+++ src/pages/Receipts.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import { Layout } from '../components/Layout'
 
 export const Receipts = () => {
   return (
-    <Layout>
-      
-    </Layout>
+    <div>Receipts</div>
   )
 }
