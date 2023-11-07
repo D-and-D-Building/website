99392fe35ca7bb265f4fe38ba03b8f43884ef895
Harrison Itotia <harrisonitotia@gmail.com>
2023-11-07 21:53:25 +0300

    change email to phone number

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
-    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
@@ -17,24 +17,27 @@ export const Login = () => {
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Sign in to your account
+                        Welcome to D&D Building
                     </h2>
+                    <h3 className="mt-6 text-center text-3xl font-semibold text-gray-900">
+                        Log in to your account
+                    </h3>
                 </div>
                 <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
                     <input type="hidden" name="remember" value="true" />
-                    <div className="rounded-md shadow-sm -space-y-px">
-                        <div>
-                            <label htmlFor="email-address" className="sr-only">Email address</label>
+                    <div className="shadow-sm space-y-5">
+                        <div className=''>
+                            <label htmlFor="phone-number" className="sr-only">Phone number</label>
                             <input
-                                id="email-address"
-                                name="email"
-                                type="email"
-                                autoComplete="email"
+                                id="phone-number"
+                                name="number"
+                                type="tel"
+                                autoComplete="number"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Email address"
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
+                                className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                placeholder="Phone number"
+                                value={phoneNumber}
+                                onChange={e => setPhoneNumber(e.target.value)}
                             />
                         </div>
                         <div>
@@ -45,7 +48,7 @@ export const Login = () => {
                                 type="password"
                                 autoComplete="current-password"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                 placeholder="Password"
                                 value={password}
                                 onChange={e => setPassword(e.target.value)}
@@ -58,11 +61,16 @@ export const Login = () => {
                             type="submit"
                             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
-                            Sign in
+                            Log in
                         </button>
                     </div>
                 </form>
             </div>
+
+            <div className="max-w-50">
+                <img src="src\assets\movieflix-logo.png" alt="" className='h-50' />
+            </div>
+
         </div>
     )
 }
