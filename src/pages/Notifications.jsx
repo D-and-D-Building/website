6e4f06a3610c24e908aba0c9f921a79883bbdd91
Harrison Itotia <harrisonitotia@gmail.com>
2023-11-08 01:01:36 +0300

    edit and update notification and profile pages

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Layout } from '../components/Layout'
-import { Personal } from './Personal'
-import { Announcements } from './Announcements'
+import { Link } from 'react-router-dom';
 
 export const Notifications = () => {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    // Fetch notifications from your API here and update the state
+    // For now, we'll use a static array of notifications
+    setNotifications([
+      { id: 1, subject: 'Rent Due', message: 'Your rent is due on 2022-03-01.', date: '2022-02-20' },
+      { id: 2, subject: 'Maintenance Update', message: 'The elevator maintenance is completed.', date: '2022-02-18' },
+      // Add more notifications as needed
+    ]);
+  }, []);
+
   return (
     <Layout >
+      <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Notifications</h1>
 
-      <div className="flex justify-around">
-        <Personal/>
-        <Announcements/>
+      <div className="mb-4">
+        {notifications.map(notification => (
+          <Link key={notification.id} to={{ pathname: `/notifications/${notification.id}`, state: { notification } }} className="block border-b border-gray-200 py-4">
+          <h2 className="text-xl font-bold">{notification.subject}</h2>
+          <p>{notification.message}</p>
+          <p className="text-sm text-gray-500">{notification.date}</p>
+        </Link>
+        ))}
       </div>
-
+    </div>
 
     </Layout>
   )
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,11 +3,21 @@ import { Layout } from '../components/Layout'
 export const Profile = () => {
   return (
     <Layout>
-      <h1>Name: Henry Kinuthia</h1>
-      <h1>Phone number: 0711223344</h1>
-      <h1>Gmail: kinuthia01@gmail.com</h1>
-      <h1>House No: 5</h1>
-      <h1>Rent Arrears: Cleared</h1>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Profile</h1>
+
+        <div className="mb-4">
+          <h2 className="text-xl font-bold">Personal Information</h2>
+          <p className="text-gray-700">Name: John Doe</p>
+          <p className="text-gray-700">Email: johndoe@example.com</p>
+          <p className="text-gray-700">Mobile number: 0712345678</p>
+          <p className="text-gray-700">Tenant since: January 2022</p>
+        </div>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Edit Profile
+        </button>
+      </div>
     </Layout>
   )
 }
